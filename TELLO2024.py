cc00155a945db0f5ff2bef1064abf74a83103ffa
AlbertoOstorga <a01552392@tec.mx>
2024-04-09 10:02:21 -0600

    Create TELLO2024.py

---
+++ TELLO2024.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+import matplotlib as plt
+import djitellopy as Tello
+
+# ------------------------------------ Camera setup
+
+tello = Tello()                        # Initiate Tello instance
+tello.connect()                        # Connect to the drone
+tello.streamon()                       # Start stream
+image = tello.get_frame_read().frame   # Get first frame
+
+# ------------------------------------ HSV bounds
+
+LOWER = np.array((88, 166, 51))
+UPPER = np.array((179, 255, 255))
